dc5c7a807c4e52a95d0c9c93faef348ca53ccf0f
strevayg <gstrevay@gmail.com>
2026-04-07 21:58:22 -0700

    updated the launch file and subscriber node to publish the front 180deg scan of the lidar (negating the behind 180deg)

--- install/scan_subscriber/lib/python3.12/site-packages/scan_subscriber/scan_listener.py
+++ install/scan_subscriber/lib/python3.12/site-packages/scan_subscriber/scan_listener.py
@@ -1,32 +1,36 @@
 import rclpy
-import math
+from math import sin
 from rclpy.node import Node
+from numpy import linspace, inf
 from sensor_msgs.msg import LaserScan
 
 class ScanListener(Node):
     def __init__(self):
         super().__init__('scan_listener')
+        self.publisher = self.create_publisher(LaserScan, '/flitered_scan', 10)
         self.subscription = self.create_subscription(LaserScan, '/scan', self.scan_callback, 10)
+        
+        self.width = .73152  # width of pavbot (m) assuming 2.4ft
+        self.extent = self.width / 2.0 # center to edge distance so lidar doesnt react to itself 
+        self.get_logger().info('Publishing filtered_scan topic. Use Rviz for visualization.')
     def indexCalculator(self,msg,angle):
         index = int((angle - msg.angle_min) / msg.angle_increment)
         index %= len(msg.ranges) # properly wraps around 
         return index 
     def scan_callback(self, msg):
-        # whatever we want it to do! test this rn with some prints
-        # this will give the front index 
+        # whatever we want it to do; return a LaserScan topic with a reduced ranges array 
+        # ranges will just give the front 180 degree frame of reference for the lidar
+        #    can be adjusted, change the indeces within points array
+
         # (directly in front where the arrow and logo RPLIDAR is)
         # angle_min = -pi    | angle_max = pi    | angle_increment = .00196 rad (.1125deg)
         # for ros2 0rad is pointing at the wires 
-        leftIndex = self.indexCalculator(msg, (3*math.pi)/2)
-        leftDist = msg.ranges[leftIndex]
-        self.get_logger().info(f"Left Distance: {leftDist:.2f} m")
-        frontIndex = self.indexCalculator(msg, math.pi) #rads (everything is in rads)
-        frontDist = msg.ranges[frontIndex]
-        self.get_logger().info(f"Front distance: {frontDist:.2f} m")
-        rightIndex = self.indexCalculator(msg, math.pi/2) # physical right
-        rightDist = msg.ranges[rightIndex]
-        self.get_logger().info(f"Right distance: {rightDist:.2f} m")
-
+        # left: 3pi/2 (270)   | front: pi (180)   | right: pi/2 (90)
+        angles = linspace(msg.angle_min, msg.angle_max, len(msg.ranges))
+        points = [r * sin(theta) if (theta <  -1.5708 or theta > 1.5708) else inf for r, theta in zip(msg.ranges, angles)]
+        newRanges = [r if abs(y) < self.extent else inf for r,y in zip(msg.ranges, points)]
+        msg.ranges = newRanges
+        self.publisher.publish(msg)
 def main():
 
     rclpy.init()
